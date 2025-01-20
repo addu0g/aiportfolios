a7748560e2c7cddd0b78c28bc783d5a3985a9e85
José Edgar Gómez Elizalde <addu0g@live.com.mx>
2025-01-20 12:49:16 -0600

    A basic app to create investments porfolios using IA

---
+++ aiportfolios/views.py
@@ -0,0 +1,33 @@
+from django.http import HttpResponse
+from django.template import loader
+import markdown
+import google.generativeai as genai
+import os
+
+def index(request):
+    template = loader.get_template("aiportfolios/index.html")
+    context = {  }
+
+    return HttpResponse(template.render(context, request))
+
+
+def iaquery(request):
+    os.environ["GOOGLE_API_KEY"] =''
+    genai.configure(api_key=os.environ['GOOGLE_API_KEY'])
+    model = genai.GenerativeModel("gemini-1.5-flash")
+    
+    template = loader.get_template("aiportfolios/iaresult.html")
+    age = request.POST["age"]
+    risk = request.POST["risk"]
+    horizon = request.POST["horizon"]
+    objective = request.POST["objective"]
+    role = "Act as Warren Buffet, Act as Peter Lynch, Act as Stanley Druckenmiller, "
+    goal = f"Generate a portfolio for a person in its {age} who is looking a risk tolerance: {risk}, and with an investment horizon of {horizon} this investment will be to {objective}" 
+    restrictions = "Use simple language, write in a formal tone"
+    format = "Answer with bullet points, include an html table"
+    prompt = f"{role} - {goal} - {restrictions} - {format}"
+    portfolioResult = model.generate_content(prompt)
+    context = {
+        "result": markdown.markdown(portfolioResult.text)
+    }
+    return HttpResponse(template.render(context, request))
