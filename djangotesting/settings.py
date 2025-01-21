b3ae5b87f4ca0d7390223377fcb8f3bcaef23657
José Edgar Gómez Elizalde <addu0g@live.com.mx>
2025-01-21 16:52:23 -0600

    cors experiments

--- djangotesting/settings.py
+++ djangotesting/settings.py
@@ -44,6 +44,7 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     # CORS
     'corsheaders.middleware.CorsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -54,7 +55,7 @@ MIDDLEWARE = [
 ]
 
 ROOT_URLCONF = 'djangotesting.urls'
-CORS_ALLOW_ALL_ORIGINS=True
+
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
