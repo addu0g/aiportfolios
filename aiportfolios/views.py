4a25b8fe8869617cf7fa3cff4158970baedb725a
José Edgar Gómez Elizalde <addu0g@live.com.mx>
2025-01-21 13:59:50 -0600

    Added bootstrap an language options

--- aiportfolios/views.py
+++ aiportfolios/views.py
@@ -4,6 +4,12 @@ import markdown
 import google.generativeai as genai
 import os
 
+def home(request):
+    template = loader.get_template("master.html")
+    context = {  }
+
+    return HttpResponse(template.render(context, request))    
+
 def index(request):
     template = loader.get_template("aiportfolios/index.html")
     context = {  }
@@ -12,7 +18,7 @@ def index(request):
 
 
 def iaquery(request):
-    os.environ["GOOGLE_API_KEY"] =''
+    
     genai.configure(api_key=os.environ['GOOGLE_API_KEY'])
     model = genai.GenerativeModel("gemini-1.5-flash")
     
@@ -21,13 +27,18 @@ def iaquery(request):
     risk = request.POST["risk"]
     horizon = request.POST["horizon"]
     objective = request.POST["objective"]
-    role = "Act as Warren Buffet, Act as Peter Lynch, Act as Stanley Druckenmiller, "
+    language = "English"
+    if "language" in request.POST:
+        language = "Spanish"
+    role = "Act as Warren Buffet, Act as Peter Lynch, Act as Stanley Druckenmiller, Act as Ray Dalio"
     goal = f"Generate a portfolio for a person in its {age} who is looking a risk tolerance: {risk}, and with an investment horizon of {horizon} this investment will be to {objective}" 
     restrictions = "Use simple language, write in a formal tone"
-    format = "Answer with bullet points, include an html table"
+    format = f"Create the response in language: {language}, Answer with bullet points, include an html table for the portfolio summary"
     prompt = f"{role} - {goal} - {restrictions} - {format}"
     portfolioResult = model.generate_content(prompt)
     context = {
         "result": markdown.markdown(portfolioResult.text)
     }
     return HttpResponse(template.render(context, request))
+
+    
--- djangotesting/urls.py
+++ djangotesting/urls.py
@@ -15,9 +15,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
+from aiportfolios import views
 from django.urls import include, path
 
 urlpatterns = [
+    path("", views.home),
     path("aiportfolios/", include("aiportfolios.urls")),
     path('admin/', admin.site.urls),
 ]
